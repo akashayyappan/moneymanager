06f1f9a32a8ee4c1a0953f6ecce653cf9d5e7b62
akashayyappan <akashayyappan@outlook.com>
2024-12-14 13:52:48 +0530

    removed env file

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb+srv://akashayyappan:BVuHcGIGPWVGtLT5@moneymanager.kcyf9.mongodb.net/?retryWrites=true&w=majority&appName=moneymanager';
-
 const initializeDB = () => {
   // Mongo DB Connection String
   const uri = process.env.MONGO_DB_URL || '';
-  return mongoose.connect(url)
+  return mongoose.connect(uri)
     .then(() => console.log('DB Connected'))
     .catch(err => console.log('Error connecting to DB', err));
 }
